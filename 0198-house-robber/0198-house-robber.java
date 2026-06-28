9337b11ee62bb4fd8af1b19bf6a0e2bea63ffb05
Anurag Tiwari <90105993+Anuurag2318@users.noreply.github.com>
2026-06-28 14:06:14 +0530

    Time: 0 ms (100%), Space: 43.1 MB (5.32%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0198-house-robber/0198-house-robber.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int robbing(int[] arr,int i,int free,int n,int [][]dp){
+        if(i==n){
+            return 0;
+        }
+        if(free==0){
+            return dp[i][free]=robbing(arr,i+1,1,n,dp);
+        }
+        if(dp[i][free]!=-1){
+            return dp[i][free];
+        }
+        int c1=arr[i]+robbing(arr,i+1,0,n,dp);
+        int c2=robbing(arr,i+1,1,n,dp);
+        return dp[i][free]=Math.max(c1,c2);
+    }
+    public int rob(int[] arr) {
+        
+        int free=1;
+        int n=arr.length;
+        int dp[][]=new int[n][2];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int i=0;
+        return robbing(arr,i,free,n,dp);
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
