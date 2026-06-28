a462e481626ff277eeb87952d5c689c09178741f
Anurag Tiwari <90105993+Anuurag2318@users.noreply.github.com>
2026-06-28 14:06:38 +0530

    Update 0198-house-robber.java

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -25,7 +25,3 @@ class Solution {
         return robbing(arr,i,free,n,dp);
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
