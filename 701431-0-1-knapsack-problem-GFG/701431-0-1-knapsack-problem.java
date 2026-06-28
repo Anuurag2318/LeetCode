57b4da8b14897059d07563ea2f196da4585657f5
Anurag Tiwari <90105993+Anuurag2318@users.noreply.github.com>
2026-06-28 14:16:30 +0530

    Added solution - LeetHub

---
+++ 701431-0-1-knapsack-problem-GFG/701431-0-1-knapsack-problem.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int knapProb(int capacity,int val[],int wt[],int n,int i,int[][]dp){
+        if(i==n){
+            return 0;
+        }
+        if(dp[i][capacity]!=-1){
+            return dp[i][capacity];
+        }
+        if(wt[i]>capacity){
+            return dp[i][capacity]=knapProb(capacity,val,wt,n,i+1,dp);
+        }
+        int c1=val[i]+knapProb(capacity-wt[i],val,wt,n,i+1,dp);
+        int c2=knapProb(capacity,val,wt,n,i+1,dp);
+        return dp[i][capacity]=Math.max(c1,c2);
+    }
+    public int knapsack(int W, int val[], int wt[]) {
+        // code here    
+        int i=0;
+        int n=val.length;
+        int dp[][]=new int[n][W+1];
+        for(int j=0;j<n;j++){
+            Arrays.fill(dp[j],-1);
+        }
+        return knapProb(W,val,wt,n,i,dp);
+    }
+}
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
