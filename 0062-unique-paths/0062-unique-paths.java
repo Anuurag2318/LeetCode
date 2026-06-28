a1900149424a6eb06c406a12d01e2b3f2b0d655b
Anurag Tiwari <90105993+Anuurag2318@users.noreply.github.com>
2026-06-28 18:02:55 +0530

    Time: 1 ms (14.06%), Space: 42.2 MB (46.95%) - LeetHub - Time: O(m*n), Space: O(m*n)

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,26 +1,23 @@
 class Solution {
-    public static int unique(int i,int j,int m,int n,int dp[][]){
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
-        if(i>=m || i<0 || j<0||  j>=n){
-            return 0;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int c1=unique(i+1,j,m,n,dp);
-        int c2=unique(i,j+1,m,n,dp);
-        return dp[i][j]=c1+c2;
-    }
+    
     public int uniquePaths(int m, int n) {
-        int dp[][]=new int[m+1][n+1];
+       int dp[][]=new int[m+1][n+1];
         for(int i=0;i<=m;i++){
-            Arrays.fill(dp[i],-1);
+            dp[i][n]=0;
         }
-        int i=0;
-        int j=0;
-        return unique(i,j,m,n,dp);
+        for(int j=0;j<=n;j++) {
+            dp[m][j] = 0;
+        }
+        dp[m-1][n-1]=1;
+        for(int i=m-1;i>=0;i--){
+            for(int j=n-1;j>=0;j--){
+                if(i==m-1 && j==n-1){
+                    continue;
+                }
+                dp[i][j]=dp[i+1][j]+dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 }
 
