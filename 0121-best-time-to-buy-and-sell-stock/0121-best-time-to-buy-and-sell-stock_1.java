d93898b65c8800353723db14e6b2738c15d0bc25
Anurag Tiwari <90105993+Anuurag2318@users.noreply.github.com>
2026-06-29 18:09:15 +0530

    Implement maxProfit function for stock trading

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock_1.java
@@ -0,0 +1,34 @@
+public static int maxProf(int[] prices,int n,int i,int transaction,int[][] dp){
+        if(i==n){
+            return 0;
+        }
+        if(transaction==0){
+            return 0;
+        }
+        if(dp[i][transaction]!=-1){
+            return dp[i][transaction];
+        }
+        if(transaction==2){
+            int c1=maxProf(prices,n,i+1,transaction-1,dp)-prices[i];
+            int c2=maxProf(prices,n,i+1,transaction,dp);
+            return dp[i][transaction]=Math.max(c1,c2);
+        }
+        else{
+            int c1=maxProf(prices,n,i+1,transaction-1,dp)+prices[i];
+            int c2=maxProf(prices,n,i+1,transaction,dp);
+            return dp[i][transaction]=Math.max(c1,c2);
+        }
+    }
+    public static int maxProfit(int[] prices) {
+        int n=prices.length;
+        int transaction=2;
+        int dp[][]=new int[n+1][transaction+1];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        int i=0;
+        return maxProf(prices,n,i,transaction,dp);
+    }
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
