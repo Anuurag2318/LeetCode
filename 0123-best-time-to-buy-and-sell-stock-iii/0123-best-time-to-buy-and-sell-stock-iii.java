68ca518aa6240782d1d398ce26537f61ba6fb9da
Anurag Tiwari <90105993+Anuurag2318@users.noreply.github.com>
2026-07-01 11:51:00 +0530

    Time: 22 ms (46.19%), Space: 94.3 MB (41.02%) - LeetHub - Time: O(, Space: O(?)

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int n=prices.length;
+        if(n==0){
+            return 0;
+        }
+        int transaction=4;
+//        for i==n
+        int [][]dp=new int[n+1][transaction+1];
+        for(int j=0;j<=transaction;j++){
+            dp[n][j]=0;
+        }
+//        For Transaction==0
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=1;j<=transaction;j++){
+                if(j%2==0){
+                    dp[i][j]=Math.max(dp[i+1][j-1]-prices[i],dp[i+1][j]);
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i+1][j-1]+prices[i],dp[i+1][j]);
+                }
+            }
+        }
+        return dp[0][4];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
