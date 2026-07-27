f2fca54928e9cc681cf77fbaed7c0b38f3b6fff8
Anurag Tiwari <90105993+Anuurag2318@users.noreply.github.com>
2026-07-27 16:05:17 +0530

    Time: 1 ms (99.96%), Space: 46.6 MB (93.04%) - LeetHub - Time: O(n), Space: O(1)

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,10 @@
 class Solution {
     public int singleNumber(int[] arr) {
-        HashMap<Integer,Integer> freq=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            freq.put(arr[i],freq.getOrDefault(arr[i],0)+1);
-        }
-        for(Integer key: freq.keySet()){
-            if(freq.get(key)==1){
-                return key;
-            }
-        }
-        return 0;
+       int x=0;
+       for(int i=0;i<arr.length;i++){
+            x=x^arr[i];
+       }
+       return x;
     }
 }
 
